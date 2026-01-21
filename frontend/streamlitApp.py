9ffb7e354c20e5c0d2d8b08f53a6506054dbafa0
Briefgarde <nvollert108@gmail.com>
2026-01-21 13:08:34 +0100

    first draft of the streamlit app

---
+++ frontend/streamlitApp.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import requests
+import json
+
+def callAPI(name:str):
+    import requests
+
+    url = "http://127.0.0.1:8000/predict"
+
+    payload = {"name": name}
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "insomnia/12.2.0"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+
+    return response.text
+
+st.title("GenderMe")
+
+name = None
+with st.form(key='form'):
+    
+    name = st.text_input('Enter a surname:')
+    submit = st.form_submit_button()
+
+if submit:
+    if not name or not name.strip():
+        st.warning('Please enter a surname')
+    else: 
+        try:
+            prediction = json.loads(callAPI(name)) 
+            
+            gender = prediction.get('gender')
+            confidence = prediction.get('confidence')
+            used_name = prediction.get('name')
+
+            if gender and confidence:
+                text = f"We think {used_name} is {str.lower(gender)} with {round(confidence*100, 2)}% confidence"
+                st.write(text)
+            else:
+                st.error("API returned incomplete data.")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+            
+
