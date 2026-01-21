ac56e3c8ef3dcae44e03fbfc13d4edaedc464456
Briefgarde <nvollert108@gmail.com>
2026-01-21 16:48:39 +0100

    passed the url to a secrets.toml file

--- frontend/streamlitApp.py
+++ frontend/streamlitApp.py
@@ -3,46 +3,13 @@ import requests
 import json
 
 def callAPI(name:str):
-    import requests
-
-    url = "https://gendermebackend-v1.onrender.com/predict"
-
+    base_url = st.secrets["RENDER_BACKEND_URL"]
+    url = base_url + "/predict"
     payload = {"name": name}
-
     response = requests.request("POST", url, json=payload)
-
     return response.text
 
-st.title("GenderMe")
-
-name = None
-with st.form(key='form'):
-    
-    name = st.text_input('Enter a surname:')
-    submit = st.form_submit_button()
-
-if submit:
-    if not name or not name.strip():
-        st.warning('Please enter a surname')
-    else: 
-        try:
-            prediction = json.loads(callAPI(name)) 
-            
-            gender = prediction.get('gender')
-            confidence = prediction.get('confidence')
-            used_name = prediction.get('name')
-
-            if gender and confidence:
-                text = f"We think {used_name} is {str.lower(gender)} with {round(confidence*100, 2)}% confidence"
-                st.write(text)
-            else:
-                st.error("API returned incomplete data.")
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
-            
-
 def fixed_footer():
-    # CSS to position the footer
     footer_style = """
     <style>
     .footer {
@@ -58,15 +25,39 @@ def fixed_footer():
     }
     </style>
     """
-    
-    # Inject CSS
     st.markdown(footer_style, unsafe_allow_html=True)
-    
-    # The footer content
+
     st.markdown(
-        '<div class="footer"><p>Do not use GenderMe for any kind of research of commercial use case.</p></div>',
+        '<div class="footer"><p>Do not use GenderMe for any kind of research or commercial use case.</p></div>',
         unsafe_allow_html=True
     )
 
+st.title("GenderMe")
 
+name = None
+with st.form(key='form'):
+    
+    name = st.text_input('Enter a surname:')
+    submit = st.form_submit_button()
+
+if submit:
+    if not name or not name.strip():
+        st.warning('Please enter a surname')
+    else: 
+        try:
+            with st.spinner("Wait for it... (Backend is asleep, the cons of the free plan)", show_time=True):
+                prediction = json.loads(callAPI(name)) 
+            
+            gender = prediction.get('gender')
+            confidence = prediction.get('confidence')
+            used_name = prediction.get('name')
+
+            if gender and confidence:
+                text = f"We think {used_name} is {str.lower(gender)} with {round(confidence*100, 2)}% confidence"
+                st.write(text)
+            else:
+                st.error("API returned incomplete data.")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+            
 fixed_footer()
