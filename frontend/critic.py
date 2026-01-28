d235e7fa2d3c5fecaca3bf8a72d4c8e36e29916f
Briefgarde <nvollert108@gmail.com>
2026-01-28 17:59:59 +0100

    changed structure to create critic tabs, content TBD

---
+++ frontend/critic.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def show_critic():
+    critic = """
+    There are things that are far from being optimal in this project. 
+    In this section, I'll attempt to give a summary of those, as well as give (concepts of) solutions to apply to those problems. 
+    """
+    st.markdown(critic)
--- frontend/streamlitApp.py
+++ frontend/streamlitApp.py
@@ -2,6 +2,7 @@ import streamlit as st
 from predictor import predictor
 from footer import fixed_footer
 from study import show_study
+from critic import show_critic
 
 st.set_page_config(
         page_title="GenderMe",
@@ -11,12 +12,15 @@ st.title("GenderMe")
 
 st.markdown("A little demo by Briefgarde. Check out the [GitHub](https://github.com/Briefgarde/genderme)")
 
-tab1, tab2 = st.tabs(["Predictor", "Study"])
+tab1, tab2, tabs3= st.tabs(["Predictor", "Study", "Critics"])
 
 with tab1:
     predictor()
 
 with tab2:
     show_study()
+
+with tabs3:
+    show_critic()
 
 fixed_footer()
